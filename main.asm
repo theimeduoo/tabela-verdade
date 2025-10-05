be4e413b92f24c32d7e8674165131cf509bfd656
theimeduoo <157843123+theimeduoo@users.noreply.github.com>
2025-10-05 00:59:37 -0300

    Adicionado a label func
    Essa parte do código adicionada é responsável por calcular o resultado das entradas e retornar na tela o valor.
    
    Então se um usuário colocar as entradas:
    A = 0
    B = 0
    C = 1
    
    O sistema irá processar os valores de acordo com a tabela verdade formada por: (A+B+C) * (A+!B+!C) * (!A+B+!C) * (!A+!B+!C)
    
    E retornar o resultado. 
    
    Porém precisa de ajustes pois o resultado não está sendo como esperado.

--- main.asm
+++ main.asm
@@ -38,3 +38,47 @@ main:
 	li $v0, 5
 	syscall
 	move $t3, $v0 #Guarda o valor de C em $t3
+
+	jal func
+	
+	# --- DEPOIS DE CALCULADO O RESULTADO --- #
+	la $a0, resultado
+	li $v0, 1
+	syscall
+	
+	move $a0, $v0
+	li $v0, 1
+	syscall
+	
+	li $v0, 10
+	syscall
+	
+	
+#(A+B+C) * (A+!B+!C) * (!A+B+!C) * (!A+!B+!C)
+
+#A = $t1
+#B = $t2
+#C = $t3
+			
+func:
+	xori $t4, $t1, 1 # $t4 = !A
+	xori $t5, $t2, 1 # $t5 = !B
+	xori $t6, $t3, 1 # $t6 = !C
+
+	or $t7, $t1, $t2 # (A + B)
+	or $t7, $t4, $t3 # (A + B) + C
+	
+	or $t8, $t1, $t5 # (A + !B)
+	or $t8, $t8, $t3 # (A + !B) + !C
+	
+	or $t9, $t4, $t2 # (!A + B)
+	or $t9, $t9, $t6 # (!A + B) + !C
+	
+	or $t0, $t4, $t5 # (!A + !B)
+	or $t0, $t0, $t6 # (!A + !B) + !C
+	
+	and $v0, $t7, $t8 # (A+B+C) * (A+!B+!C)
+	and $v0, $v0, $t9 # (A+B+C) * (A+!B+!C) * (!A+B+!C)
+	and $v0, $v0, $t0 # (A+B+C) * (A+!B+!C) * (!A+B+!C) * (!A+!B+!C)
+	
+	jr $ra
